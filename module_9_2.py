05e934df0ea81c443ee97a77af667b92e2240f87
Vladickush <u.vn@mail.ru>
2024-10-02 01:06:29 +0300

    Add files via upload

---
+++ module_9_2.py
@@ -0,0 +1,13 @@
+# Cписочные и словарные сборки
+
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+
+first_result = [len(i) for i in first_strings if len(i) >= 5]
+print(first_result)
+
+second_result = [tuple([i, j]) for i in first_strings for j in second_strings if len(i) == len(j)]
+print(second_result)
+
+third_result = {i: len(i) for i in first_strings + second_strings if not len(i) % 2}
+print(third_result)
