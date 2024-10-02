d36ff4be8e3e9a808714ea969d59dbfdc0689d76
Vladickush <u.vn@mail.ru>
2024-10-02 22:37:17 +0300

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,10 @@
+# Задача "Генераторные сборки"
+
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (len(i) - len(j) for i, j in zip(first, second) if len(i) != len(j))
+print(list(first_result))
+
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
+print(list(second_result))
